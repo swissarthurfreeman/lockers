7e9babc5cce42e892afc1ae550013abc76ad699a
A. Freeman <arthur.freemna@etu.unige.ch>
2022-07-15 19:12:24 +0200

    added basic persistence tests

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,44 @@
 import express from "express";
+import { Sequelize, DataTypes, Model } from "sequelize";
+
+const sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: 'localhost',
+    password: 'test',
+    username: 'root',
+    database: 'lockers',
+    port: 3306
+});
 
 console.log("Hello World");
 const app = express();
-const port = 8081;
+const port = 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+const User = sequelize.define('User', {
+    firstName: {  // Model attributes are defined here
+        type: DataTypes.STRING,
+        allowNull: false // allowNull defaults to true
+    },
+    lastName: {
+        type: DataTypes.STRING
+    }
+})
+
+async function main() {
+    await sequelize.sync({ force: true });
+    console.log("All models were synchronized successfully.");
+
+    app.listen(port, () => {
+        console.log(`Server started at http://localhost:${port}`);
+    });
+}
+
+app.get("/", async (req, res) => {
+    const user = await User.create({"firstName": "Steve", "lastName": "Rody"});
+    res.send(user);
 });
 
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
-});
+
+main();
 
 export { app };
